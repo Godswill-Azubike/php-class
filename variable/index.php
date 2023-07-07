0d36a8cd5025ac69f6c273ee9dee3425e9b6c1ff
Godswill Azubike <43970405+Godswill-Azubike@users.noreply.github.com>
2023-07-07 20:16:52 +0100

    chore: variables, types and operators

--- variable/index.php
+++ variable/index.php
@@ -1,2 +1,33 @@
 <?php
-// nothing
+// Variables in php
+// Data types
+// operators
+
+// data types
+//    int => integer
+//    string => collection characters 
+//    bool => true / false
+//    double/float => numbers with decimal place 4.5
+
+
+// operators 
+// assignment op =
+// mathematical op * - / + %
+// logical op || ! && 
+// comparison < > >= <= != == ===
+
+$name;
+
+$name = "Godswill";
+
+
+
+$age = 58;
+$child_age = 18;
+$work = "we work";
+$temp = 20.01;
+
+$result =  $child_age - $age;
+
+echo $name;
+echo $result;
